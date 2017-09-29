0365e968801f664bacd21544b70e6efd6431117c
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2017-09-29 23:21:28 +1300

    Update start_nvr.sh

--- start_nvr.sh
+++ start_nvr.sh
@@ -1,3 +1,8 @@
+if [ ! -f /var/lib/unifi-video/system.properties ]; then
+        echo "System Properties NOT found - creating new install"
+        cp -R /tmp/unifi-video/* /var/lib/unifi-video/
+fi
+
 service mongodb start
 service unifi-video start
 tail -F /var/log/unifi-video/server.log
