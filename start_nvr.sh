c2a59dc2c383ce974cfa7af00f6b8244155b18ff
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2017-09-30 11:25:01 +1300

    Update start_nvr.sh

--- start_nvr.sh
+++ start_nvr.sh
@@ -1,6 +1,6 @@
 if [ ! -f /usr/lib/unifi-video/system.properties ]; then
         echo ""
-        echo "System Properties NOT found - creating new install"
+        echo "System Properties file NOT found - creating new install"
         cp -R /tmp/unifi-video/* /usr/lib/unifi-video/ 
         chown -R unifi-video:unifi-video /var/lib/unifi-video
         chown root:root /var/lib/unifi-video/videos
