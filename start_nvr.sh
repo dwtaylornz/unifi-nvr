8cc9bda14ad4ebafc39043d0bfd317446bf1cb6e
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2017-10-01 12:42:16 +1300

    Update start_nvr.sh

--- start_nvr.sh
+++ start_nvr.sh
@@ -4,8 +4,15 @@ if [ ! -f /var/lib/unifi-video/system.properties ]; then
         echo "System Properties file NOT found - creating new install"
         cp -R /tmp/unifi-video/* /var/lib/unifi-video/ 
         chown -R unifi-video:unifi-video /var/lib/unifi-video
-        chown -R unifi-video:unifi-video /var/log/unifi-video
         # chown root:root /var/lib/unifi-video/videos
+        echo ""
+fi
+
+#check if logs directory has anything in it? - if not set permissions on log dir 
+if [ ! -f /var/log/unifi-video/server.log ]; then
+        echo ""
+        echo "Logs directory contain zero files - setting permissions
+        chown -R unifi-video:unifi-video /var/log/unifi-video
         echo ""
 fi
 
