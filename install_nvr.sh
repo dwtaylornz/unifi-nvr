4916a89b0cf033c291c9b0471b7d7c9486aeac54
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2017-10-01 17:06:08 +1300

    Update install_nvr.sh

--- install_nvr.sh
+++ install_nvr.sh
@@ -1,13 +1,12 @@
 # download and install latest ubuntu package
-# wget -q -O unifi-video.deb https://dl.ubnt.com/firmwares/ufv/v3.8.0/unifi-video.Ubuntu14.04_amd64.v3.8.0.deb
 wget -q -O unifi-video.deb https://dl.ubnt.com/firmwares/ufv/v3.8.0/unifi-video.Ubuntu16.04_amd64.v3.8.0.deb
 dpkg -i unifi-video.deb
 apt-get -f install -y
 
 # remove symbolic links 
-#cd /usr/lib/unifi-video
-#rm data 
-#rm logs 
+cd /usr/lib/unifi-video
+rm data 
+rm logs 
 
 # set start_nvr.sh executable 
 chmod +x /start_nvr.sh
