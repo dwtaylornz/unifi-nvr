3c4b61e1c8045e18a21e40785b9511d50472fc7d
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2017-10-01 16:38:39 +1300

    Update start_nvr.sh

--- start_nvr.sh
+++ start_nvr.sh
@@ -1,23 +1,18 @@
+echo "--- START NVR ---" 
+
 # check if clean install - if so popluate directory with required files 
-if [ ! -f /var/lib/unifi-video/system.properties ]; then
-        echo ""
-        echo "System Properties file NOT found - creating new install"
-        # cp -R /tmp/unifi-video/* /var/lib/unifi-video/ 
-        chown -R unifi-video:unifi-video /var/lib/unifi-video
-        cd /usr/lib/unifi-video/ 
-        rm data 
-        ln -s /var/lib/unifi-video/ data
-        # chown root:root /var/lib/unifi-video/videos
-        echo ""
+if [ ! -f /usr/lib/unifi-video/data/system.properties ]; then
+        echo "Data directory contains zero files - setting permissions"
+        chown -R unifi-video:unifi-video /usr/lib/unifi-video/data
 fi
 
 #check if logs directory has anything in it? - if not set permissions on log dir 
-if [ ! -f /var/log/unifi-video/server.log ]; then
-        echo ""
+if [ ! -f /usr/lib/unifi-video/logs/server.log ]; then
         echo "Logs directory contain zero files - setting permissions"
-        chown -R unifi-video:unifi-video /var/log/unifi-video
-        echo ""
+        chown -R unifi-video:unifi-video /var/log/unifi-video    
 fi
+
+echo "" 
 
 # service mongodb start
 service unifi-video start
