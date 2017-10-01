ae78086e5161624caeaf8de260af1a232cfa134c
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2017-10-01 16:43:18 +1300

    Update start_nvr.sh

--- start_nvr.sh
+++ start_nvr.sh
@@ -9,7 +9,7 @@ fi
 #check if logs directory has anything in it? - if not set permissions on log dir 
 if [ ! -f /usr/lib/unifi-video/logs/server.log ]; then
         echo "Logs directory contain zero files - setting permissions"
-        chown -R unifi-video:unifi-video /var/log/unifi-video    
+        chown -R unifi-video:unifi-video /usr/lib/unifi-video/logs    
 fi
 
 echo "" 
