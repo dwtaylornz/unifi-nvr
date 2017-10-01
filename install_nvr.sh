d56d41467b960cb098f65af78723babe2ee12f07
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2017-10-01 13:13:28 +1300

    Update install_nvr.sh

--- install_nvr.sh
+++ install_nvr.sh
@@ -1,10 +1,3 @@
-# update apt-get repositories and refresh system
-apt-get update
-
-# install pre-reqs
-apt-get -f install -y wget apt-utils
-apt-get -f install -y openjdk-8-jre-headless
-
 # download and install latest ubuntu package
 wget -q -O unifi-video.deb https://dl.ubnt.com/firmwares/ufv/v3.8.0/unifi-video.Ubuntu14.04_amd64.v3.8.0.deb
 dpkg -i unifi-video.deb
