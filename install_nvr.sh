975e26cd90bc267e283a332479637457a286fe34
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2018-01-11 15:44:00 +1300

    Update install_nvr.sh

--- install_nvr.sh
+++ install_nvr.sh
@@ -1,7 +1,8 @@
 # download and install latest ubuntu package
 # wget -q -O unifi-video.deb https://dl.ubnt.com/firmwares/ufv/v3.8.2/unifi-video.Ubuntu16.04_amd64.v3.8.2.deb
 # wget -q -O unifi-video.deb https://dl.ubnt.com/firmwares/ufv/v3.8.3/unifi-video.Ubuntu16.04_amd64.v3.8.3.deb
-wget -q -O unifi-video.deb https://dl.ubnt.com/firmwares/ufv/v3.8.5/unifi-video.Ubuntu16.04_amd64.v3.8.5.deb
+# wget -q -O unifi-video.deb https://dl.ubnt.com/firmwares/ufv/v3.8.5/unifi-video.Ubuntu16.04_amd64.v3.8.5.deb
+wget -q -O unifi-video.deb https://dl.ubnt.com/firmwares/ufv/v3.9.0/unifi-video.Ubuntu16.04_amd64.v3.9.0.deb
 dpkg -i unifi-video.deb
 apt-get -f install -y
 
